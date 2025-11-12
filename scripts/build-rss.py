f41ac503f7410bdda882a209392aebbc57a5c06a
duong tam kien <tk@renard.ninja>
2025-11-12 18:48:32 +0100

    démarre le notebook

---
+++ scripts/build-rss.py
@@ -0,0 +1,20 @@
+import marimo
+
+__generated_with = "0.17.7"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import pandas as pd
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
